3590256fc7c889a2f3d7c2a34f49537c7dc6b9b4
Skippa-mX <korolevmihail877@gmail.com>
2023-05-15 20:16:06 +0300

    123

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+    <custom-button @click='goToPages("/")'>bebe</custom-button>
+    <custom-button @click='goToPages("/characters")'>bebe</custom-button>
+    <custom-button @click='goToPages("/about")'>bebe</custom-button>
+  </div>
+</template>
+
+<script>
+
+import router from "@/router/router";
+
+export default {
+  name: "navBar",
+  methods:{
+    goToPages(path){
+      router.push(path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UI/index.js
@@ -0,0 +1,8 @@
+import customButton from "@/components/UI/CustomButton";
+import customInput from "@/components/UI/CustomInput";
+
+export default
+[
+    customInput,
+    customButton,
+]
